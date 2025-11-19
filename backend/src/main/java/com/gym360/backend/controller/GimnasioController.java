d60a4d0e319c22f874802b83af5703b5d82949c6
xabiondo <xgarciaber@educacion.navarra.es>
2025-11-19 12:58:42 +0100

    empiezo controller gimnasio

--- backend/src/main/java/com/gym360/backend/controller/GimnasioController.java
+++ backend/src/main/java/com/gym360/backend/controller/GimnasioController.java
@@ -1,15 +1,28 @@
 package com.gym360.backend.controller;
 
 
+import com.gym360.backend.model.Gimnasio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("api/gimnasios")
 @CrossOrigin("http://localhost:5173")
 public class GimnasioController {
+
+    public ResponseEntity<ArrayList<Gimnasio>> gimnasiosCercanos(@RequestParam double latitud , double longuitud){
+
+        
+
+    }
+
+
 
 
 }
